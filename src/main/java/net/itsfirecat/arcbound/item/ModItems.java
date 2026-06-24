0463c284138d4ef11ef82e114a40313c0dec5bad
cat <firecat9074@gmail.com>
2026-06-24 22:16:29 +0300

    move to root

--- src/main/java/net/itsfirecat/arcbound/item/ModItems.java
+++ src/main/java/net/itsfirecat/arcbound/item/ModItems.java
@@ -12,17 +12,31 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item FREEZE_SHARD = registerItem("freeze_shard", new Item(new Item.Settings()));
     public static final Item FREEZE_ARTIFACT = registerItem("freeze_artifact", new Item(new Item.Settings()));
-    public static final Item FREEZE_ARC = registerItem("freeze_arc", new FreezeArcItem(new Item.Settings()));
+
+    public static final Item FREEZE_ARC = registerItem("freeze_arc",
+            new FreezeArcItem(new Item.Settings().maxCount(1).fireproof()));
+
     public static final Item INFINITY_ARTIFACT = registerItem("infinity_artifact", new Item(new Item.Settings()));
-    public static final Item INFINITY_ARC = registerItem("infinity_arc", new InfinityArcItem(new Item.Settings()));
+
+    public static final Item INFINITY_ARC = registerItem("infinity_arc",
+            new InfinityArcItem(new Item.Settings().maxCount(1).fireproof()));
+
     public static final Item PULSE_ARTIFACT = registerItem("pulse_artifact", new Item(new Item.Settings()));
-    public static final Item PULSE_ARC = registerItem("pulse_arc", new PulseArcItem(new Item.Settings()));
+
+    public static final Item PULSE_ARC = registerItem("pulse_arc",
+            new PulseArcItem(new Item.Settings().maxCount(1).fireproof()));
+
     public static final Item DASH_SHARD = registerItem("dash_shard", new Item(new Item.Settings()));
     public static final Item DASH_ARTIFACT = registerItem("dash_artifact", new Item(new Item.Settings()));
-    public static final Item DASH_ARC = registerItem("dash_arc", new DashArcItem(new Item.Settings()));
+
+    public static final Item DASH_ARC = registerItem("dash_arc",
+            new DashArcItem(new Item.Settings().maxCount(1).fireproof()));
+
     public static final Item RESONANCE_SHARD = registerItem("resonance_shard", new Item(new Item.Settings()));
     public static final Item RESONANCE_ARTIFACT = registerItem("resonance_artifact", new Item(new Item.Settings()));
-    public static final Item RESONANCE_ARC = registerItem("resonance_arc", new ResonanceArcItem(new Item.Settings()));
+
+    public static final Item RESONANCE_ARC = registerItem("resonance_arc",
+            new ResonanceArcItem(new Item.Settings().maxCount(1).fireproof()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(arcbound.MOD_ID, name), item);
@@ -34,12 +48,17 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
             entries.add(FREEZE_SHARD);
             entries.add(FREEZE_ARTIFACT);
+            entries.add(FREEZE_ARC);
             entries.add(INFINITY_ARTIFACT);
+            entries.add(INFINITY_ARC);
             entries.add(PULSE_ARTIFACT);
+            entries.add(PULSE_ARC);
             entries.add(DASH_SHARD);
             entries.add(DASH_ARTIFACT);
+            entries.add(DASH_ARC);
             entries.add(RESONANCE_SHARD);
             entries.add(RESONANCE_ARTIFACT);
+            entries.add(RESONANCE_ARC);
         });
     }
 }
